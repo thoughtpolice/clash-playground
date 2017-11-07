ea202b857585f76a87d17a1986b4c49aebf2eb92
Austin Seipp <aseipp@pobox.com>
2017-11-06 19:53:53 -0600

    nix: become more pious by absolving ourselves of NIX_PATH's sins
    See https://nixos.wiki/wiki/How_to_fetch_Nixpkgs_with_an_empty_NIX_PATH
    
    Signed-off-by: Austin Seipp <aseipp@pobox.com>

--- nix/nixpkgs.nix
+++ nix/nixpkgs.nix
@@ -1,11 +1,6 @@
 { config }:
 
 let
-  bootstrap = import <nixpkgs> {};
   nixpkgs   = builtins.fromJSON (builtins.readFile ./nixpkgs.json);
-  src       = bootstrap.fetchFromGitHub {
-    owner  = "nixos";
-    repo   = "nixpkgs";
-    inherit (nixpkgs) rev sha256;
-  };
+  src       = import ./fetchnix.nix { inherit (nixpkgs) rev sha256; };
 in import src { inherit config; }
